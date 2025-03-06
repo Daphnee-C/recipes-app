0c4425ed241602a784e47d67d42f7fbea927d9cc
Daphnee-C <dadacc16@gmail.com>
2025-03-06 14:15:44 +0100

    recipies app server back

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import connectDB from './client/db.js'
+import 'dotenv/config'
+import usersRouter from './routes/usersRouter.js'
+
+const app = express()
+
+app.use('/api', usersRouter)
+
+app.get('/', (req,res) => {
+    res.end('Test')
+})
+
+connectDB();
+
+app.listen(3000, () => {
+    console.log(`server is running on port 3000`)
+
+})
